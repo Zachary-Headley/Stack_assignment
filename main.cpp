b12d78656fe134023f0a095ec436232098271079
Zachary-Headley <143136232+Zachary-Headley@users.noreply.github.com>
2024-05-04 05:30:34 +0000

    lol

--- main.cpp
+++ main.cpp
@@ -12,12 +12,19 @@ class Stack{
             stack = {};
         }
 
+    // Adds a char to the stack
+
         void push(char top){
             stack.push_back(top);
         }
+
+    // returns how many parenthesis are in the stack
+
         int getSize(){
             return stack.size();
         }
+    // Makes sure there is something to pop, then removes top of stack if there is something there.
+
         void pop(){
             int size = getSize();
             if(size == 0){
@@ -26,6 +33,9 @@ class Stack{
             }
             stack.pop_back();
         }
+
+    // Makes sure something is there, then returns the top of the stack
+
         char peek() {
             int size = getSize();
             if(size == 0){
@@ -33,12 +43,15 @@ class Stack{
                 return 'o';
             }
             return stack[size - 1];
-            cout << stack[size-1];
         }
+
+    // Checks to see if the parenthesis match one another
 
          bool match(char open, char close) {
             return (open == '(' && close == ')') || (open == '[' && close == ']') || (open == '{' && close == '}');
         }
+
+    //Makes sure the string from main is balanced, and returns the appropriate error if parenthesis are not balanced.
 
         void isBalanced() {
             Stack check;
@@ -47,16 +60,15 @@ class Stack{
                     check.push(c); // Push opening parentheses onto the stack
                 } else if (c == ')' || c == ']' || c == '}') {
                         char open = check.peek(); // Get the most recent opening parenthesis
-                        // pop(); // Pop the opening parenthesis from the stack
-                        if (!match(open, c)) {
+                        if (!match(open, c)) { // Checks to see if they equal one another
                             cout << "Unbalanced: Mismatched parentheses" << endl;
                             return;
                         } else {
-                            check.pop();
+                            check.pop(); // pops top parenthesis
                         }
                  }
             }
-            if (check.getSize() > 0) {
+            if (check.getSize() > 0) { //checks to see if there is any parenthesis left over
                 cout << "Unbalanced: Extra parenthesis" << endl;
                 return;
             } else {
@@ -66,7 +78,10 @@ class Stack{
         }
 };
 
-int main(){
+//Creates a string, and inputs the string char by char into the stack balance.
+//Then checks if the stack is balanced.
+
+int main(){ 
     string s;
     Stack balance;
     cout << "Enter your parenthesis: ";
